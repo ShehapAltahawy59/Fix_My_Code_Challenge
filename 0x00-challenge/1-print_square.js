0114932ec06851d26374c7822875e79eea65f949
shehap62 <shehapkhalil62@gmail.com>
2024-03-22 14:51:56 +0200

    edit

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -1,24 +1,22 @@
 #!/usr/bin/node
+
 /*
-    Print a square with the character #
-    
-    The size of the square must be the first argument 
-    of the program.
+  Print a square with the character #
+  The size of the square must be provided as the first argument.
 */
 
-
 if (process.argv.length <= 2) {
-    process.stderr.write("Missing argument\n");
-    process.stderr.write("Usage: ./1-print_square.js <size>\n");
-    process.stderr.write("Example: ./1-print_square.js 8\n");
-    process.exit(1)
-}
-
-let size = parseInt(process.argv[2], 10)
-
-for (let i = 0 ; i < size ; i ++) {
-    for (let j = 0 ; j < size ; j ++) {
-        process.stdout.write("#");
+    console.error("Missing argument"); // Use console.error for clearer output
+    console.error("Usage: node 1-print_square.js <size>"); // Use node to run
+    console.error("Example: node 1-print_square.js 8");
+    process.exit(1);
+  }
+  
+  const size = parseInt(process.argv[2], 10); // Use const for variables that don't change
+  
+  for (let i = 0; i < size; i++) {
+    for (let j = 0; j < size; j++) {
+      process.stdout.write("#");
     }
-    process.stdout.write("\n");
-}
+    console.log(); // Use console.log for line breaks
+  }
