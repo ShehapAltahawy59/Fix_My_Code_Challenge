061ac46fc2bc741d506a1737d1139c9fad358f34
shehap62 <shehapkhalil62@gmail.com>
2024-03-22 14:54:11 +0200

    edit

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -6,17 +6,18 @@
 */
 
 if (process.argv.length <= 2) {
-    console.error("Missing argument"); // Use console.error for clearer output
-    console.error("Usage: node 1-print_square.js <size>"); // Use node to run
+    console.error("Missing argument");
+    console.error("Usage: node 1-print_square.js <size>");
     console.error("Example: node 1-print_square.js 8");
     process.exit(1);
   }
   
-  const size = parseInt(process.argv[2], 10); // Use const for variables that don't change
+  const size = parseInt(process.argv[2], 10); // Base-10 conversion
   
   for (let i = 0; i < size; i++) {
     for (let j = 0; j < size; j++) {
       process.stdout.write("#");
     }
-    console.log(); // Use console.log for line breaks
+    console.log(); // Newline
   }
+  
